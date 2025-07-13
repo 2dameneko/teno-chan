dd996b5313ce7c1cccd449d912d347ee55a3bfc9
2dameneko <sort.of.kannagi@gmail.com>
2025-07-13 17:45:02 +0500

    Updated default CUDA_VISIBLE_DEVICES env

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 rem It is better to use direct indication of the order of devices used, just as specified in your cmd when starting llama-cpp
 rem set CUDA_VISIBLE_DEVICES=0,1
-set CUDA_VISIBLE_DEVICES=0
+set CUDA_VISIBLE_DEVICES=0,1
 
 call venv\Scripts\activate
 python teno-chan.py ^
